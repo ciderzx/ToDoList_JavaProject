b89f58af78d5f39f0353d256a7d52806be64f51f
SeungWoo <ciderzx@gmail.com>
2020-06-15 01:58:31 +0900

    스크롤 추가 구현

--- app/src/main/java/com/example/todolist_javaproject/ScrollActivity.java
+++ app/src/main/java/com/example/todolist_javaproject/ScrollActivity.java
@@ -1,14 +1,30 @@
 package com.example.todolist_javaproject;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ScrollActivity  extends AppCompatActivity {
 
+    public Button add_view;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scroll);
+
+        add_view = findViewById(R.id.add_layout);
+
+        add_view.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                SubList n_layout = new SubList(getApplicationContext());
+                LinearLayout con = (LinearLayout)findViewById(R.id.con);
+                con.addView(n_layout);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/todolist_javaproject/SubList.java
@@ -0,0 +1,25 @@
+package com.example.todolist_javaproject;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.LinearLayout;
+
+public class SubList extends LinearLayout {
+    public SubList(Context context) {
+        super(context);
+
+        init(context);
+    }
+
+    public SubList(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        init(context);
+    }
+
+    private void init(Context context){
+        LayoutInflater inflater =(LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        inflater.inflate(R.layout.activity_sublayout,this,true);
+    }
+}
